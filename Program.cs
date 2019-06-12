34b45a2967b466ffdea04a465979cc92dbd624e7
Williams Argenal <46799764+crazyWilliams@users.noreply.github.com>
2019-06-11 22:12:13 -0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+               
+                StartSequence();
+            }
+            catch (Exception a)
+            {
+                
+            }
+            finally
+            {
+                Console.WriteLine("Program is complete");
+            }
+        }
+
+        static void StartSequence()
+        {
+
+            // console welcoming  user to name 
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+            Console.WriteLine("Please enter a number greater than zero");
+
+            // user inputs number 
+            string answer = Console.ReadLine();
+            int number = Convert.ToInt32(answer);
+            int[] userArray = new int[number];
+         //   Populate(userArray);
+           // GetSum(userArray);
+
+          
+        }
+       /* static int[] Populate(int[] userArray)
+        {
+
+        }
+
+        static int GetSum(int[] sumArray)
+        {
+
+        }
+        */
+
+    }
+}
